dc07fd19148a30770a20b4aab1f88d2d8198e70d
aceberry924 <meobius123@gmail.com>
2021-05-09 20:27:14 -0400

    Update currency utils

--- src/utils/currency.js
+++ src/utils/currency.js
@@ -2,4 +2,5 @@ export const currency = (n, curr = 'EUR', LanguageFormat = 'es') =>
   Intl.NumberFormat(LanguageFormat, {
     style: 'currency',
     currency: curr,
+    minimumFractionDigits: 0,
   }).format(n);
