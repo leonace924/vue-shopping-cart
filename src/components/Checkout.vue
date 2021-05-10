6ca867e2f4daaa0d39c587c6151a3b6dcdc7b035
aceberry924 <meobius123@gmail.com>
2021-05-09 20:27:28 -0400

    Update checkout component with data

--- src/components/Checkout.vue
+++ src/components/Checkout.vue
@@ -1,25 +1,57 @@
 <template>
-  <div class="w-64 px-8 py-10 bg-gray-100 rounded-r xl:w-72">
+  <div class="flex flex-col w-full px-8 py-10 bg-gray-100 rounded-r lg:h-full lg:w-64 xl:w-72">
     <h1 class="pb-4 text-lg font-medium text-black border-b border-gray-300">Order Summary</h1>
-    <div class="flex py-8 border-b border-gray-300">
-      <span class="text-sm text-gray-700"> {{ cartSize }} Items </span>
+    <div class="flex justify-between py-8 border-b border-gray-300">
+      <span class="text-sm text-gray-700">{{ cartSize }} Items</span>
+      <span class="text-base font-semibold">{{ currency(cartOriginalPrice) }}</span>
     </div>
-    <div class="flex py-6 border-b border-gray-300">
-      <span class="text-xs text-gray-500 uppercase">
-        Discount
+    <div class="py-6 border-b border-gray-300">
+      <span class="block mb-5 text-xs text-gray-500 uppercase">
+        Discounts
       </span>
+
+      <div class="grid gap-5">
+        <div v-for="rule in discountRules || []" :key="rule.name" class="flex justify-between">
+          <span class="text-sm text-gray-700">{{ rule.name }}</span>
+          <span class="text-sm font-semibold">
+            {{ currency(discountProduct(rule.product)) }}
+          </span>
+        </div>
+      </div>
+    </div>
+
+    <div class="mt-auto">
+      <div class="flex items-center justify-between py-6 border-t border-gray-300">
+        <span class="text-sm text-gray-700 uppercase">
+          Total Cost
+        </span>
+        <span class="text-xl font-bold text-gray-700">
+          {{ currency(cartTotalPrice) }}
+        </span>
+      </div>
+      <button
+        class="w-full p-4 mb-6 text-base font-semibold leading-3 text-white bg-indigo-600 rounded focus:outline-none"
+      >
+        Checkout
+      </button>
     </div>
   </div>
 </template>
 
 <script>
+import { currency } from '@/utils/currency';
 import { mapState, mapGetters } from 'vuex';
 
 export default {
   name: 'checkout',
   computed: {
-    ...mapState(['cart']),
-    ...mapGetters(['cartSize', 'cartTotalAmount']),
+    ...mapState(['cart', 'discountRules']),
+    ...mapGetters(['cartSize', 'cartOriginalPrice', 'cartTotalPrice', 'discountProduct']),
+  },
+  setup() {
+    return {
+      currency,
+    };
   },
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,30 @@
 <template>
   <div
-    class="w-full h-screen bg-dark-purple bg-cover"
+    class="w-full h-screen bg-cover bg-dark-purple"
     :style="{
       backgroundImage: 'url(' + require('@/assets/images/background.png') + ')',
     }"
   >
     <div
-      class="bg-white rounded w-full md:w-3/4 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 m-6"
+      class="fixed max-w-5xl transform -translate-x-1/2 -translate-y-1/2 bg-white rounded w-modal md:w-5/6 top-1/2 left-1/2 h-modal max-h-160"
     >
-      <Cart />
+      <div class="h-full overflow-auto lg:flex">
+        <Cart />
+        <Checkout />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import Cart from '@/components/Cart';
+import Checkout from '@/components/Checkout';
 
 export default {
   name: 'home',
   components: {
     Cart,
+    Checkout,
   },
 };
 </script>
